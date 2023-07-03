2469858bb1afba02ac6f8c583cf748627b834c14
gogovono17 <106349938+gogovono17@users.noreply.github.com>
2023-07-03 16:55:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+namespace _1._Valid_Usernames
+{
+    using System;
+    using System.Text;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine().Split(", ");
+
+            foreach(string username in usernames)
+            {
+                bool checker = true;
+                if(username.Length >= 3 && username.Length <= 16)
+                {
+                    foreach(char chr in username)
+                    {
+                        if(!(char.IsLetterOrDigit(chr) || chr == '-' || chr == '_'))
+                        {
+                            checker = false;
+                            break;
+                        }
+                    }
+                    if (checker)
+                    {
+                        Console.WriteLine(username);
+                    }
+                    
+                }
+                
+            }
+        }
+    }
+}
